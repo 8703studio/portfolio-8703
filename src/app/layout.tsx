ffe1006656dd98f4a04b73f85f3ffc0a9ad50ec7
8703 Studio <72730252+8703studio@users.noreply.github.com>
2024-04-24 05:39:41 +0200

    adding google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
+import { DM_Sans } from 'next/font/google'
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 import "./globals.css"
+
+// If loading a variable font, you don't need to specify the font weight
+const dmSans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-dm-sans',
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr" className={dmSans.variable}>
       <body className="bg-red-500">{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
