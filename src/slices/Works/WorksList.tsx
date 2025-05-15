4d3bf412b856096e4209d68cb1da16095a4706cd
8703 Studio <72730252+8703studio@users.noreply.github.com>
2025-05-15 04:33:18 +0200

    update MAY 15-2

--- src/slices/Works/WorksList.tsx
+++ src/slices/Works/WorksList.tsx
@@ -12,24 +12,22 @@ export async function WorksList({ id }: Props) {
     const client = createClient();
     const works = await client.getByID<Content.WorksDocument>(id);
     return (
-        <div className="group relative mx-auto w-full max-w-72 px-8 pt-4 ">
-
-            <div className="-mb-1 overflow-hidden py-4">
-                <PrismicLink field={works.data.link_project}>
-                    <PrismicNextImage
-                        alt=""
-                        field={works.data.image}
-                        className=" mx-auto w-full origin-top"
-                    />
-                </PrismicLink>
-            </div>
-            <div className="flex flex-col justify-between ~text-sm/2xl">
-                <h3 className="my-2 font-sans leading-tight ~text-lg/xl">
-                    titre
-                </h3>
-                <span className="inline-flex items-center gap-1">
-                    category
-                </span>
+        <div className="group relative mx-auto w-full">
+            <div className="grid gap-6 w-full">
+                <div className="relative cursor-pointer">
+                    <PrismicLink field={works.data.link_project}>
+                        <PrismicNextImage
+                            alt=""
+                            field={works.data.image}
+                            className=" mx-auto w-full origin-top"
+                        />
+                    </PrismicLink>
+                    <div className="absolute top-0 left-0 px-6 py-4">
+                        <h4 className="mb-3 font-semibold tracking-tight text-white">This is the title</h4>
+                    </div>
+                    <div className='absolute bottom-3 px-6 py-4'>
+                    <span className="leading-normal text-gray-100">category</span></div>
+                </div>
             </div>
         </div>
     )
--- src/slices/Works/index.tsx
+++ src/slices/Works/index.tsx
@@ -29,8 +29,8 @@ const Works = ({ slice }: WorksProps): JSX.Element => {
           <PrismicRichText field={slice.primary.body} />
         </div>
         <>
-        <div className="mt-16">            
-          <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <div className="mt-16 p-5 md:p-18">            
+          <div className="grid w-full grid-cols gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
               {slice.primary.works.map(({ projects }) => (
                 isFilled.contentRelationship(projects) && (
                   <WorksList key={projects.id} id={projects.id} />
